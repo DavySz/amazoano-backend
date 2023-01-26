ddbab278d94bf51bd3fb31fca4b00bbc4f654ce7
davy <davydesouzabar@gmail.com>
2023-01-25 22:19:46 -0400

    chore: config typeorm

--- src/modules/wishlists/wishlists.module.ts
+++ src/modules/wishlists/wishlists.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { WishlistsController } from './controllers/wishlists.controller';
+import { Wishlist } from './entities/wishlist.entity';
 import { WishlistsService } from './services/wishlists.service';
 
 @Module({
   controllers: [WishlistsController],
   providers: [WishlistsService],
+  imports: [TypeOrmModule.forFeature([Wishlist])],
 })
 export class WishlistsModule {}
