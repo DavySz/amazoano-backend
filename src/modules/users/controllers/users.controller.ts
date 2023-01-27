5c22525ee7c31d851eb812d23ad2a9232d61aa20
davy <davydesouzabar@gmail.com>
2023-01-26 23:14:28 -0400

    feat: create users controller

---
+++ src/modules/users/controllers/users.controller.ts
@@ -0,0 +1,27 @@
+import { Body, Controller, Delete, Param, Patch, Post } from '@nestjs/common';
+import { CreateUserDTO } from '../dtos/create-user.dto';
+import { UpdateUserDTO } from '../dtos/update-user.dto';
+import { UsersService } from '../services/users.service';
+
+@Controller('users')
+export class UsersController {
+  constructor(private readonly usersService: UsersService) {}
+
+  @Post('create')
+  create(@Body() createUserDTO: CreateUserDTO) {
+    return this.usersService.create(createUserDTO);
+  }
+
+  @Patch('update/:userId')
+  update(
+    @Param('userId') userId: string,
+    @Body() updateUserDTO: UpdateUserDTO,
+  ) {
+    return this.usersService.update(userId, updateUserDTO);
+  }
+
+  @Delete('delete/:userId')
+  delete(@Param('userId') userId: string) {
+    return this.usersService.delete(userId);
+  }
+}
