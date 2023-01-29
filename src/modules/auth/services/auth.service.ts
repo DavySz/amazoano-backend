c400b23484249c31e8e51ffe8de71a5f02d900d8
davy <davydesouzabar@gmail.com>
2023-01-29 16:26:46 -0400

    feat: create auth email password method

--- src/modules/auth/services/auth.service.ts
+++ src/modules/auth/services/auth.service.ts
@@ -1,41 +1,54 @@
 import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
 import { compare } from 'bcrypt';
+import { IUserPayload } from 'src/modules/users/interfaces/user-payload';
 import { UsersService } from 'src/modules/users/services/users.service';
-import { AuthenticationByEmailAndPasswordDTO } from '../dtos/authenticationByEmailAndPassword.dto';
-import { TValidateUserResponse } from '../interfaces/validate-user';
+import { IAuthEmailPasswordDTO } from '../dtos/auth-email-password.dto';
+import { IAuthEmailAndPassword } from '../interfaces/auth-email-password';
+import { IValidateUser } from '../interfaces/validate-user';
 
 @Injectable()
 export class AuthService {
-  constructor(private readonly usersService: UsersService) {}
+  constructor(
+    private readonly usersService: UsersService,
+    private readonly jwtService: JwtService,
+  ) {}
 
-  private async validateUser({
-    email,
-    password,
-  }: AuthenticationByEmailAndPasswordDTO): Promise<TValidateUserResponse> {
-    const user = await this.usersService.findUserByEmail(email);
+  async validateUser(email: string, password: string): Promise<IValidateUser> {
+    const user = await this.usersService.findByEmail(email);
     const passwordMatch = await compare(password, user.password);
 
     if (!user || !passwordMatch) {
-      throw new UnauthorizedException('email or password incorrect');
+      throw new UnauthorizedException(
+        'Email address or password provided is incorrect.',
+      );
     }
 
     return {
-      user: {
-        first_name: user.first_name,
-        last_name: user.last_name,
-        userId: user.userId,
-        phone: user.phone,
-        email: user.email,
-      },
-      accessToken: '',
+      first_name: user.first_name,
+      last_name: user.last_name,
+      userId: user.userId,
+      phone: user.phone,
+      email: user.email,
     };
   }
 
-  async authenticationByEmailAndPassword({
-    email,
-    password,
-  }: AuthenticationByEmailAndPasswordDTO): Promise<TValidateUserResponse> {
-    const userValidated = await this.validateUser({ email, password });
-    return userValidated;
+  async authEmailPassword(
+    userDTO: IAuthEmailPasswordDTO,
+  ): Promise<IAuthEmailAndPassword> {
+    const payload: IUserPayload = {
+      sub: userDTO.userId,
+      email: userDTO.email,
+      name: userDTO.first_name,
+    };
+
+    const jwtToken = this.jwtService.sign(payload);
+
+    return {
+      access_token: jwtToken,
+      user: {
+        ...userDTO,
+      },
+    };
   }
 }
